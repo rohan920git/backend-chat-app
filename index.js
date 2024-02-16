57e81d9d8cb4127273d754ce30beddc1aae9dffe
Rohan <rohanb158@gmail.com>
2024-02-16 21:43:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import{Server} from "socket.io"
+import cors from 'cors'
+import http from 'http'
+import dotenv from 'dotenv'
+dotenv.config();
+const app  = express();
+const server = http.createServer(app);
+app.use(cors({origin:"*"}));
+app.get("/hey" , (req ,res)=>{
+    res.send("<h1>hey</h1>")
+})  
+const io = new Server({cors:{
+ origin:"*",
+}}).listen(server)
+io.on("connection",(socket)=>{
+    console.log("users connected" + socket.id)
+    socket.on("send_message",(message)=>{
+        io.emit("receive_message",`id:${socket.id} ->${message}`)
+    })
+   
+})
+
+
+  server.listen(process.env.PORT,process.env.HOST,()=>{
+    console.log(`server started on ${process.env.PORT}`)
+})

a935b6761e82c4d4cece7371f0ba66e50c7e0e49
Rohan <rohanb158@gmail.com>
2024-02-18 01:36:49 +0530

    ready for deployment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
@@ -20,15 +21,26 @@ console.log(__dirname)
 const server =   app.listen(PORT,()=>{
     console.log(`server started on ${process.env.PORT}`)
 })
-const io = new Server(server,{cors:{
+const io = new Server(server,{
+    cors:{
  origin:"*",
-}})
+}
+,  methods: ['GET', 'POST'],
+})
 io.on("connection",(socket)=>{
     console.log("users connected" + socket.id)
     socket.on("send_message",(message)=>{
-        console.log("from backend "+ message)
-        socket.broadcast.emit("receive_message",message)
+        console.log("from backend "+ message.data)
+        socket.to(message.room).emit("receive_message",{data:message.data,by:socket.id})
       
+    })
+    socket.on("join-room",(room)=>{
+        console.log(socket.id + "joined", room)
+        socket.join(room)
+    })
+    socket.on("leave-room",(room)=>{
+        console.log(socket.id + " is left")
+        socket.leave(room);
     })
     socket.on("disconnect",()=>{
         console.log("user disconnected")

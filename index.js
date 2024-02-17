8cca79dda0948c59fda9907193eb3a04019ffa9d
Rohan <rohanb158@gmail.com>
2024-02-17 10:28:31 +0530

    some changes

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 import express from "express";
 import{Server} from "socket.io"
 import cors from 'cors'
-import http from 'http'
 import dotenv from 'dotenv'
+import path,{dirname} from 'path'
+import { fileURLToPath } from 'url';
+
 
 dotenv.config();
 const PORT = process.env.PORT || 8080;
 const app  = express();
-const server = http.createServer(app);
+
 app.use(cors({origin:"*"}));
-app.get("/hey" , (req ,res)=>{
-    res.send("<h1>hey</h1>")
-})  
-const io = new Server({cors:{
+
+app.use(express.static('./out'))
+
+const server =   app.listen(PORT,"0.0.0.0",()=>{
+    console.log(`server started on ${process.env.PORT}`)
+})
+const io = new Server(server,{cors:{
  origin:"*",
-}}).listen(server)
+}})
 io.on("connection",(socket)=>{
     console.log("users connected" + socket.id)
     socket.on("send_message",(message)=>{
-        io.emit("receive_message",`:-${message}`)
+        console.log("from backend "+ message)
+        socket.broadcast.emit("receive_message",message)
+      
     })
+    socket.on("disconnect",()=>{
+        console.log("user disconnected")
+    })
+    socket.on('reconnect_attempt', () => {
+        console.log('Attempting to reconnect to Socket.IO');
+      });
    
 })
-  server.listen(PORT,process.env.HOST,()=>{
-    console.log(`server started on ${process.env.PORT}`)
-})
+

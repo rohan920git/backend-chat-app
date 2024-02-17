3d1620785a2b1855d1d4aa8bba8818134fdb4cb9
Rohan <rohanb158@gmail.com>
2024-02-18 02:15:53 +0530

    some more changes bd5

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ const server =   app.listen(PORT,()=>{
 })
 const io = new Server(server,{
     cors:{
- origin:"*",
-}
-,  methods: ['GET', 'POST'],
+        origin: 'http://backend-chat-app-omega.vercel.app',
+        methods: ['GET', 'POST'],
+},
 })
 io.on("connection",(socket)=>{
     console.log("users connected" + socket.id)
